056343e25dba39f78ebd2d7123d5f9ed66f284e8
Hoang Duc <minhduchoang2303@gmail.com>
2025-04-26 15:18:10 +0700

    first commit

---
+++ client/src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <ModeToggle />
+      Register Page aaa
+    </div>
+  );
+};
+
+export default RegisterPage;
