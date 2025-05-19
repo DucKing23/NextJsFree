841952466291b6304bf4e375bef40c6e6595fbf3
Hoang Duc <minhduchoang2303@gmail.com>
2025-05-19 13:38:39 +0700

    auth

--- client/src/me/page.tsx
+++ client/src/me/page.tsx
@@ -6,25 +6,25 @@ export default async function MeProfile() {
   const cookieStore = await cookies();
   const sessionToken = cookieStore.get("theme");
   console.log(sessionToken);
-  //   const result = await fetch(
-  //     `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
-  //     {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${sessionToken}`,
-  //       },
-  //     }
-  //   ).then(async (res) => {
-  //     const payload = await res.json();
-  //     const data = {
-  //       status: res.status,
-  //       payload,
-  //     };
-  //     if (!res.ok) {
-  //       throw data;
-  //     }
-  //     return data;
-  //   });
-
+  const result = await fetch(
+    `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessionToken?.value}`,
+      },
+    }
+  ).then(async (res) => {
+    const payload = await res.json();
+    const data = {
+      status: res.status,
+      payload,
+    };
+    if (!res.ok) {
+      throw data;
+    }
+    return data;
+  });
+  console.log(result);
   return <div>me do</div>;
 }
